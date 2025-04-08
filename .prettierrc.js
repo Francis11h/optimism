c1472584b62ae180faf08e1570718d8bde387968
Francis <710892027dongyi@gmail.com>
2025-04-08 19:02:04 +0800

    OptimismPortalStop

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   $schema: 'http://json.schemastore.org/prettierrc',
   plugins: [],
   trailingComma: 'es5',
-  tabWidth: 2,
+  tabWidth: 4,
   semi: false,
   singleQuote: true,
   arrowParens: 'always',
